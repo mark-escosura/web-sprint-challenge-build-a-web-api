1434ab8222b567ece7770df806d0d881426979e3
mark-escosura <escosuramarkse@gmail.com>
2021-12-03 15:01:32 -0800

    Tenth Commit: [GET] actions endpoint is now completed.

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,11 +1,18 @@
 // Write your "actions" router here!
 const express = require("express");
 
-
-// const Actions = require("./actions-model.js");
+const Actions = require("./actions-model.js");
 
 const router = express.Router();
 
-
+// [GET] Returns an array of actions (or an empty array) as the body of the response.
+router.get("/", async (req, res, next) => {
+  try {
+    const actions = await Actions.get();
+    res.status(200).json(actions);
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;

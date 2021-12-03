fc0716e31737b205ddbde90f8f6fe7148da6775c
mark-escosura <escosuramarkse@gmail.com>
2021-12-03 00:15:30 -0800

    Third Commit: Environment Variables Section is Completed.

--- index.js
+++ index.js
@@ -12,3 +12,15 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Pull your server into this file and start it!
 */
+require('dotenv').config()
+
+const express = require('express');
+
+const server = express();
+
+// PORT
+const PORT = process.env.PORT || 9000
+
+server.listen(PORT, () => {
+    console.log(`server is listening on ${PORT}`)
+})

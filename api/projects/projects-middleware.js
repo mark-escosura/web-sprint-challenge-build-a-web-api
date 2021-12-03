9bff88e6d3176e25216a87558a5e89fac5f57d24
mark-escosura <escosuramarkse@gmail.com>
2021-12-03 13:02:54 -0800

    Fifth Commit: [GET] by project id Endpoint, is now completed.

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,27 @@
 // add middlewares here related to projects
+const Project = require("../projects/projects-model.js");
+
+async function validateProjectId(req, res, next) {
+  const { id } = req.params;
+  try {
+    const project = await Project.get(id);
+    if (project) {
+      req.project = project; // saves other middlewares a db trip
+      next();
+    } else {
+      res.status(404).json({
+        message: "Error getting Project id",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "Error getting Project ID",
+      error: err.message,
+    });
+    next();
+  }
+}
+
+module.exports = {
+  validateProjectId,
+};
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,5 +1,6 @@
 // Write your "projects" router here!
 const express = require("express");
+const { validateProjectId } = require("./projects-middleware");
 
 // middleware
 
@@ -8,14 +9,20 @@ const Projects = require("./projects-model");
 const router = express.Router();
 // [GET] Returns an array of projects as the body of the response.
 router.get("/", async (req, res) => {
-    try {
-        const project = await Projects.get();
-        res.status(200).json(project);
-    } catch (err) {
-        res.status(500).json({
-            message: "Error"
-        })
-    }
-})
+  const projects = await Projects.get();
+  try {
+    res.status(200).json(projects);
+  } catch (err) {
+    res.status(500).json({
+      message: "Error",
+      error: err.message,
+    });
+  }
+});
 
-module.exports = router
+// [GET] Returns a project with the given `id` as the body of the response.
+router.get("/:id", validateProjectId, async (req, res) => {
+  res.status(200).json(req.project);
+});
+
+module.exports = router;

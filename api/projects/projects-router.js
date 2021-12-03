c8647a23213b44f9fc34808df2e0a0511faf63e6
mark-escosura <escosuramarkse@gmail.com>
2021-12-03 12:32:01 -0800

    Fourth Commit: [GET] Endpoint is now completed and passing.

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,21 @@
 // Write your "projects" router here!
+const express = require("express");
+
+// middleware
+
+const Projects = require("./projects-model");
+
+const router = express.Router();
+// [GET] Returns an array of projects as the body of the response.
+router.get("/", async (req, res) => {
+    try {
+        const project = await Projects.get();
+        res.status(200).json(project);
+    } catch (err) {
+        res.status(500).json({
+            message: "Error"
+        })
+    }
+})
+
+module.exports = router
